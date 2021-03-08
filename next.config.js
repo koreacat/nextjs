432fe66b93dacfb9b7dda62fede3b27ca66c64e7
지경민 <jgm0531@midasit.com>
2021-03-08 18:40:24 +0900

    axios

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ module.exports = {
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
+    images: {
+        loader: 'imgix'
+    },
     env: {
         USER: process.env.USER,
         PASS: process.env.PASS

4decf77b1aba7bc246a1b9ca6d02c7555a32d134
지경민 <jgm0531@midasit.com>
2021-06-16 03:27:35 +0900

    heartFill

--- pages/components.tsx
+++ pages/components.tsx
@@ -2,6 +2,7 @@ import WaterFill from "../src/components/waterFill";
 import Count from "../src/components/count/index.";
 import Layout from "../src/components/common/layout";
 import {useEffect, useState} from "react";
+import HeartFill from "../src/components/heartFill";
 
 const Components = () => {
 	const [averagePos, setAveragePos] = useState(Math.floor(Math.random() * 101));
@@ -24,6 +25,7 @@ const Components = () => {
 
 	return (
 		<Layout>
+			<Count count={count}/>
 			<div style={{display: "flex"}}>
 				<WaterFill
 					title={'waterFill'}
@@ -32,16 +34,32 @@ const Components = () => {
 				/>
 				<WaterFill
 					title={'waterFill'}
-					type={'VALUE'}
-					average={averageVal}
-				/>
-				<WaterFill
-					title={'waterFill'}
 					type={'PERFORMANCE'}
 					average={averagePer}
 				/>
+				<WaterFill
+					title={'waterFill'}
+					type={'VALUE'}
+					average={averageVal}
+				/>
 			</div>
-			<Count count={count}/>
+			<div style={{display: "flex"}}>
+				<HeartFill
+					title={'면접온도 지수'}
+					type={'POSITIVE'}
+					average={averagePos}
+				/>
+				<HeartFill
+					title={'면접온도 지수'}
+					type={'PERFORMANCE'}
+					average={averagePer}
+				/>
+				<HeartFill
+					title={'면접온도 지수'}
+					type={'VALUE'}
+					average={averageVal}
+				/>
+			</div>
 		</Layout>
 	)
 };

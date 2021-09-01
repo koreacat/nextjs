16610338477db09117ffa94af597cc804eb1b6bc
지경민 <kj9966@naver.com>
2021-09-02 00:15:33 +0900

    Revert "KoreaMap"
    This reverts commit 8896c2ce4c250c9cb5ae916f70a98444058373ec.

--- src/store/koreaMapUIStore.ts
+++ src/store/koreaMapUIStore.ts
@@ -1,30 +1,3 @@
-import { action, observable } from "mobx";
-
-export const LOCATION_DATA = [
-    '서울특별시', '경기도', '인천광역시', '부산광역시', 
-    '대구광역시', '광주광역시', '대전광역시', '울산광역시', 
-    '세종특별자치시', '강원도', '충청남도', '경상남도', 
-    '경상북도', '전라남도', '전라북도', '충청북도',
-    '제주특별자치도'
-];
-
 export default class KoreaMapUIStore {
-    @observable private _selectedLocations = new Set<string>();
-
-    get selectedLocations () {
-        return this._selectedLocations;
-    }
-
-    isSelected = (name: string) => {
-        return this._selectedLocations.has(name);
-    }
-
-    @action
-    toggleLocation = (name: string) => {
-        console.log(name, this._selectedLocations);
-        this._selectedLocations.has(name) 
-        ? this._selectedLocations.delete(name) 
-        : this._selectedLocations.add(name)
-    }
-
+    
 }

e00fd926778958aba84648fc2e4c2a856ec24fa7
koreacat <kj9966@naver.com>
2021-10-19 18:48:19 +0900

    selectList

--- src/pages/selectList.tsx
+++ src/pages/selectList.tsx
@@ -9,41 +9,137 @@ import { ALL_LOCATIONS, LocationNameMap } from "@domain/location";
 const DesignJobs = ['UX 디자이너', 'UI,GUI 디자이너', '웹 디자이너', '그래픽 디자이너', '모바일 디자이너', 'BI/BX 디자이너', '광고 디자이너', '제품 디자이너', '영상,모션 디자이너', '3D 디자이너', '아트 디렉터', '캐릭터 디자이너', '일러스트레이터', '2D 디자이너'];
 const DevJobs = ['프론트', '백앤드', '서버', '보안'];
 
-const JobSelect = () => {
-    const [selectedJobList, setSelectedJobList] = useState<Array<string>>([]);
+const EmptySelect = () => {
+    return <span>희망 직군을 선택해주세요.</span>
+}
+
+interface IJobSelect {
+    title: string;
+    deleteable?: boolean;
+    onDelete: () => void;
+    selectedTask: string;
+    setSelectedTask: (task: string) => void;
+    selectedJobList: string[];
+    setSelectedJobList: (selectedJobList: string[]) => void;
+}
+
+const JobSelect = (
+    {
+        title,
+        deleteable = false,
+        onDelete,
+        selectedTask,
+        setSelectedTask,
+        selectedJobList,
+        setSelectedJobList,
+    }: IJobSelect) => {
+
+    const taskList = ['design', 'dev'];
     const [jobList, setJobList] = useState([]);
 
     const handleOnChange = (value: string) => {
-        setSelectedJobList([]);
+        selectedJobList = [];
         switch (value) {
             case 'design':
                 setJobList(DesignJobs);
+                setSelectedTask('design');
                 break;
             case 'dev':
                 setJobList(DevJobs);
+                setSelectedTask('dev');
                 break;
         }
     }
 
+    const taskListEl = taskList.map(task => {
+        return <option key={task} value={task}>{task}</option>
+    })
+
     return (
         <div>
-            <select onChange={e => handleOnChange(e.target.value)} defaultValue=''>
-                <option value='' disabled hidden>매칭 희망 직군을 선택해 주세요.</option>
-                <option value='design'>디자인</option>
-                <option value='dev'>개발</option>
-            </select>
-            <SelectList
-                list={jobList}
-                selectList={selectedJobList}
-                setSelectList={setSelectedJobList}
-            />
-            <button onClick={() => console.log(selectedJobList)}>선택</button>
+            <hr/>
+            <strong>{title}</strong>
+            {deleteable && <button onClick={onDelete}>삭제</button>}
+            <div>
+                <span>매칭 희망 직군</span>
+                <select onChange={e => handleOnChange(e.target.value)} defaultValue=''>
+                    <option value='' disabled hidden>매칭 희망 직군을 선택해 주세요.</option>
+                    {taskListEl}
+                </select>
+            </div>
+            <div>
+                <span>매칭 희망 직무</span>
+                {
+                    selectedTask ?
+                    <SelectList
+                        list={jobList}
+                        selectList={selectedJobList}
+                        setSelectList={setSelectedJobList}
+                    />
+                    :
+                    <EmptySelect/>
+                }
+            </div>
         </div>
     )
 }
 
+interface JobSelect {
+    key: number;
+    title: string;
+    deleteable: boolean;
+    selectedTask: string;
+    selectedJobList: string[];
+}
+
 const SelectListPage = () => {
-    const [locationList, setLocationList] = useState<Array<string>>([]);
+    const [jobSelectList, setJobSelectList] = useState<JobSelect[]>([{
+        key: 0,
+        title: '매칭 희망 직군 직무',
+        deleteable: false,
+        selectedTask: '',
+        selectedJobList: [],
+    }]);
+    const [locationList, setLocationList] = useState<string[]>([]);
+
+    const handleAddJobSelect = () => {
+        const key = Math.random();
+        setJobSelectList(prev => [...prev, {
+            key: key,
+            title: '직군 직무 등록' + key,
+            deleteable: true,
+            selectedTask: '', 
+            selectedJobList: [],
+        }]);
+    }
+
+    const handleDeleteJobSelect = (key: number) => {
+        setJobSelectList(prev => prev.filter((jobSelect) => jobSelect.key !== key));
+    }
+
+    const setSelectedTask = (key: number) => {
+        return (value: string) => setJobSelectList(prev => prev.map(
+            jobSelect => jobSelect.key === key ? 
+            {...jobSelect, selectedTask: value} : jobSelect));
+    }
+
+    const setSelectedJobList = () => {
+        
+    }
+
+    const jobSelectListEl = jobSelectList.map((jobSelect) => {
+        const {key, title, deleteable, selectedTask, selectedJobList} = jobSelect;
+        return <JobSelect
+            key={key}
+            title={title} 
+            deleteable={deleteable}
+            onDelete={() => handleDeleteJobSelect(key)}
+            selectedTask={selectedTask}
+            setSelectedTask={setSelectedTask(key)}
+            selectedJobList={selectedJobList}
+            setSelectedJobList={setSelectedJobList}
+        />
+    });
 
     return (
         <Layout>
@@ -51,17 +147,17 @@ const SelectListPage = () => {
             <FixedHeader />
             <Contents>
                 <div style={{ width: '500px' }}>
-                    <JobSelect/>
-                    <hr />
-                    <JobSelect/>
+                    {jobSelectListEl}
+                    <button onClick={handleAddJobSelect}>직군 직무 추가</button>
                     <hr/>
+                    {/* <strong>희망 근무 지역</strong>
                     <SelectList
                         list={ALL_LOCATIONS.map(location => LocationNameMap[location])}
                         selectList={locationList}
                         setSelectList={setLocationList}
                         itemStyle={{ width: '116px', padding: '18px 0' }}
-                    />
-                    <button onClick={() => console.log(locationList)}>선택</button>
+                    /> */}
+                    <button onClick={() => console.log(jobSelectList, locationList)}>다음</button>
                 </div>
             </Contents>
         </Layout>

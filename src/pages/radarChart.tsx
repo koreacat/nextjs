de076f4bac2ddca91361ea70e7b1f0d8ec8bae91
지경민 <kj9966@naver.com>
2022-01-24 14:43:20 +0900

    [radarChart]interval 추가

--- src/pages/radarChart.tsx
+++ src/pages/radarChart.tsx
@@ -3,16 +3,31 @@ import Header from "@components/header";
 import FixedHeader from "@components/fixedHeader";
 import Contents from "@components/contents";
 import RadarChart from "src/components/charts/radarChart";
+import {useEffect, useState} from "react";
 
 const RadarChartPage = () => {
+  let count = 0;
+  const [num, setNum] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      count += 1;
+      if(count > 100) clearInterval(interval);
+      setNum(count);
+    }, 10);
+
+    return (() => {
+      clearInterval(interval);
+    })
+  }, []);
 
   const radarChartDataList = [
-    {title: '보상', values: [80, 100], emphasized: false},
-    {title: '사회가치', values: [50, 100], emphasized: true},
-    {title: '자존감', values: [70, 90], emphasized: false},
-    {title: '성장성', values: [60, 90], emphasized: false},
-    {title: '업무환경', values: [100, 70], emphasized: false},
-    {title: '안정성', values: [90, 70], emphasized: false},
+    {title: '보상', values: [num / 1.4, 100], emphasized: false},
+    {title: '사회가치', values: [num / 1.3, 100], emphasized: true},
+    {title: '자존감', values: [num / 1.8, 90], emphasized: false},
+    {title: '성장성', values: [num / 1.5, 90], emphasized: false},
+    {title: '업무환경', values: [num / 1.2, 70], emphasized: false},
+    {title: '안정성', values: [num / 1.1, 70], emphasized: false},
   ];
 
   return (

4dd1115444c02f9d4789e8b6807411a3a10c9bab
koreacat <kj9966@naver.com>
2022-10-05 14:40:41 +0900

    line chart

--- src/pages/lineChart.tsx
+++ src/pages/lineChart.tsx
@@ -1,12 +1,48 @@
 import Layout from "@components/layout";
 import Contents from "@components/contents";
-import LineChart from "src/components/charts/lineChart";
+import LineChart, { ChartData } from "src/components/charts/lineChart";
 
 const LineChartPage = () => {
+  const getColumns = () => ['0명', '100명', '200명', '300명', '400명', '500명', '600명', '700명'];
+  
+  const getRows = () => ['21.01', '21.02', '21.03', '21.04', '21.05', '21.06'];
+
+  const getChartData = (): ChartData[] => {
+    return [
+      {
+        column: 0,
+      },
+      {
+        column: 1,
+      },
+      {
+        column: 2,
+      },
+      {
+        column: 3,
+      },
+      {
+        column: 4,
+      },
+      {
+        column: 5,
+      },
+
+    ];
+  };
+
   return (
     <Layout>
       <Contents>
-        <LineChart/>
+        <div style={{width: '600px', height: '340px'}}>
+          <LineChart 
+            rows={getRows()}
+            columns={getColumns()}
+            data={getChartData()}
+            viewCount={6}
+            onIndex={1}
+          />
+        </div>
         {/* <RoundedLineChart/> */}
       </Contents>
     </Layout>
